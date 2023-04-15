46a1777a62cf70b1c6c09f83f6f8a490e7d814fa
Tomasz Roszczynialski <roszcz@gmail.com>
2023-04-15 13:42:45 +0200

    bump version 0.1.9 -> 0.1.10

--- fortepyan/__init__.py
+++ fortepyan/__init__.py
@@ -5,7 +5,7 @@ from fortepyan.midi.structures import MidiFile, MidiPiece
 from fortepyan.audio.render import midi_to_mp3, midi_to_wav
 
 __all__ = ["roll", "MidiFile", "MidiPiece", "midi_to_wav", "midi_to_mp3"]
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 # Pretty MIDI will throw an unwanted error for large files with high PPQ
 # This is a workaround
