4881436bc7714988f4f5635c6f375f2da4359694
Tomasz Roszczynialski <roszcz@gmail.com>
2023-04-30 18:04:32 +0200

    add builders for piano for ai dataset

--- fortepyan/web/database.py
+++ fortepyan/web/database.py
@@ -19,22 +19,48 @@ def make_s3_client():
 def get_user_records(user_id: int, engine) -> pd.DataFrame:
     query = f"""
         SELECT
-            b.key,
-            b.filename,
-            b.checksum,
-            c.created_at,
-            c.duration,
-            c.number_of_notes,
+            blob.key,
+            blob.filename,
+            blob.checksum,
+            midi_record.created_at,
+            midi_record.duration,
+            midi_record.number_of_notes,
             u.email
         FROM
-            active_storage_blobs AS b
+            active_storage_blobs AS blob
         INNER JOIN
-            active_storage_attachments AS a ON b.id = a.blob_id
+            active_storage_attachments AS asa ON blob.id = asa.blob_id
         INNER JOIN
-            midi_records AS c ON a.record_id = c.id
+            midi_records AS midi_record ON asa.record_id = midi_record.id
         INNER JOIN
-            users AS u ON c.user_id = u.id
-        WHERE a.record_type = 'MidiRecord' AND c.user_id = {user_id}
+            users AS u ON midi_record.user_id = u.id
+        WHERE asa.record_type = 'MidiRecord' AND midi_record.user_id = {user_id}
+    """
+    df = pd.read_sql(query, con=engine)
+    return df
+
+
+def get_all_records(engine) -> pd.DataFrame:
+    query = """
+        SELECT
+            blob.key,
+            blob.filename,
+            blob.checksum,
+            midi_record.id as record_id,
+            midi_record.created_at,
+            midi_record.duration,
+            midi_record.number_of_notes,
+            u.email,
+            u.username,
+            u.id as user_id
+        FROM
+            midi_records AS midi_record
+        INNER JOIN
+            active_storage_attachments AS asa ON midi_record.id = asa.record_id
+        INNER JOIN
+            active_storage_blobs AS blob ON asa.blob_id = blob.id
+        INNER JOIN
+            users AS u ON midi_record.user_id = u.id
     """
     df = pd.read_sql(query, con=engine)
     return df
---
+++ scripts/dataset_builders/pianoforai_base.py
@@ -0,0 +1,59 @@
+import os
+
+import pandas as pd
+from tqdm import tqdm
+from datasets import Dataset
+
+from fortepyan import MidiFile
+from fortepyan import config as C
+from fortepyan.web import database as db
+
+bucket = "piano-for-ai"
+
+
+def main():
+    engine = db.make_engine()
+    s3_client = db.make_s3_client()
+
+    df = db.get_all_records(engine)
+
+    records = make_pianoforai_records(df, s3_client)
+
+    dataset = Dataset.from_list(records)
+
+    dataset_name = "roszcz/pianofor-ai-base"
+
+    dataset.push_to_hub(repo_id=dataset_name, token=C.HF_TOKEN, split="train")
+
+
+def make_pianoforai_records(
+    df: pd.DataFrame,
+    s3_client,
+) -> list[dict]:
+    records = []
+
+    for it, row in tqdm(df.iterrows(), total=df.shape[0], desc="Building Piano For AI records"):
+        savepath = f"/tmp/{row.filename}"
+        s3_client.download_file(Bucket=bucket, Key=row.key, Filename=savepath)
+
+        mf = MidiFile(savepath, apply_sustain=False)
+        cc = mf._midi.instruments[0].control_changes
+        cc_frame = pd.DataFrame(
+            {
+                "number": [c.number for c in cc],
+                "value": [c.value for c in cc],
+                "time": [c.time for c in cc],
+            }
+        )
+        record = {
+            "notes": mf.df,
+            "control_changes": cc_frame,
+            "midi_filename": row.filename,
+            "record_id": row.record_id,
+            "user_id": row.user_id,
+            "user": row.username,
+        }
+        records.append(record)
+        os.remove(savepath)
+
+    return records
