e43b0cbfd35ba2a5c31194d695a21f3599accc5a
Tomasz Roszczynialski <roszcz@gmail.com>
2023-07-24 20:12:37 +0200

    dual roll video

--- fortepyan/view/__init__.py
+++ fortepyan/view/__init__.py
@@ -1,9 +1,10 @@
 from fortepyan.view.pianoroll.dual import draw_dual_pianoroll
-from fortepyan.view.animation.main import make_piano_roll_video
 from fortepyan.view.pianoroll.main import draw_pianoroll_with_velocities
+from fortepyan.view.animation.main import make_dual_roll_video, make_piano_roll_video
 
 __all__ = [
     "draw_dual_pianoroll",
+    "make_dual_roll_video",
     "make_piano_roll_video",
     "draw_pianoroll_with_velocities",
 ]
--- fortepyan/view/animation/main.py
+++ fortepyan/view/animation/main.py
@@ -2,6 +2,7 @@ import subprocess
 
 from fortepyan.audio.render import midi_to_mp3
 from fortepyan.midi.structures import MidiPiece
+from fortepyan.view.animation import dualroll as dualroll_animation
 from fortepyan.view.animation import pianoroll as pianoroll_animation
 
 
@@ -12,6 +13,31 @@ def make_piano_roll_video(
     cmap: str = "PuBuGn",
 ):
     scene = pianoroll_animation.PianoRollScene(piece, title=title, cmap=cmap)
+    mp3_path = midi_to_mp3(piece.to_midi())
+
+    scene_frames_dir = scene.render_mp()
+    command = f"""
+        ffmpeg -y -f image2 -framerate 30 -i {str(scene_frames_dir)}/10%4d.png\
+        -loglevel quiet -i {mp3_path} -map 0:v:0 -map 1:a:0\
+        {movie_path}
+    """
+    print("Rendering a movie to file:", movie_path)
+    subprocess.call(command, shell=True)
+
+
+def make_dual_roll_video(
+    piece: MidiPiece,
+    movie_path: str,
+    title: str = "animation",
+    base_cmap: str = "PuBuGn",
+    marked_cmap: str = "Reds",
+):
+    scene = dualroll_animation.DualRollScene(
+        piece=piece,
+        title=title,
+        base_cmap=base_cmap,
+        marked_cmap=marked_cmap,
+    )
     mp3_path = midi_to_mp3(piece.to_midi())
 
     scene_frames_dir = scene.render_mp()
