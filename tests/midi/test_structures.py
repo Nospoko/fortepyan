4a6eb1719a1d93b709093f009c9f124f36f11e1a
Samuel Janas <sneakyinsect@gmail.com>
2023-11-06 15:17:44 +0100

    switch test to pytest

--- tests/midi/test_structures.py
+++ tests/midi/test_structures.py
@@ -1,47 +1,54 @@
-import unittest
-
+import pytest
 import pandas as pd
 
 from fortepyan.midi.structures import MidiPiece
 
 
-class TestMidiPiece(unittest.TestCase):
-    def test_with_start_end_duration(self):
-        # All three columns provided
-        df = pd.DataFrame({"pitch": [60, 62], "start": [0, 1], "end": [1, 2], "duration": [1, 1], "velocity": [100, 100]})
-        piece = MidiPiece(df=df)
-        self.assertEqual(piece.df.shape[0], 2)
-
-    def test_with_start_end(self):
-        # Only start and end provided
-        df = pd.DataFrame({"pitch": [60, 62], "start": [0, 1], "end": [1, 2], "velocity": [100, 100]})
-        piece = MidiPiece(df=df)
-        self.assertTrue("duration" in piece.df.columns)
-
-    def test_with_start_duration(self):
-        # Only start and duration provided
-        df = pd.DataFrame({"pitch": [60, 62], "start": [0, 1], "duration": [1, 1], "velocity": [100, 100]})
-        piece = MidiPiece(df=df)
-        self.assertTrue("end" in piece.df.columns)
-
-    def test_with_end_duration(self):
-        # Only end and duration provided
-        df = pd.DataFrame({"pitch": [60, 62], "end": [1, 2], "duration": [1, 1], "velocity": [100, 100]})
-        piece = MidiPiece(df=df)
-        self.assertTrue("start" in piece.df.columns)
-
-    def test_missing_velocity(self):
-        # Missing velocity
-        df = pd.DataFrame({"pitch": [60, 62], "start": [0, 1], "end": [1, 2], "duration": [1, 1]})
-        with self.assertRaises(ValueError):
-            MidiPiece(df=df)
-
-    def test_missing_pitch(self):
-        # Missing pitch
-        df = pd.DataFrame({"start": [0, 1], "end": [1, 2], "duration": [1, 1], "velocity": [100, 100]})
-        with self.assertRaises(ValueError):
-            MidiPiece(df=df)
+# Define a single comprehensive fixture
+@pytest.fixture
+def df_full():
+    return pd.DataFrame(
+        {
+            "pitch": [60, 62],
+            "start": [0, 1],
+            "end": [1, 2],
+            "duration": [1, 1],
+            "velocity": [100, 100],
+        }
+    )
 
 
-if __name__ == "__main__":
-    unittest.main()
+# Tests using the fixture and modifying it according to the test's need
+def test_with_start_end_duration(df_full):
+    piece = MidiPiece(df=df_full)
+    assert piece.df.shape[0] == 2
+
+
+def test_with_start_end(df_full):
+    df_mod = df_full.drop(columns=["duration"])
+    piece = MidiPiece(df=df_mod)
+    assert "duration" in piece.df.columns
+
+
+def test_with_start_duration(df_full):
+    df_mod = df_full.drop(columns=["end"])
+    piece = MidiPiece(df=df_mod)
+    assert "end" in piece.df.columns
+
+
+def test_with_end_duration(df_full):
+    df_mod = df_full.drop(columns=["start"])
+    piece = MidiPiece(df=df_mod)
+    assert "start" in piece.df.columns
+
+
+def test_missing_velocity(df_full):
+    df_mod = df_full.drop(columns=["velocity"])
+    with pytest.raises(ValueError):
+        MidiPiece(df=df_mod)
+
+
+def test_missing_pitch(df_full):
+    df_mod = df_full.drop(columns=["pitch"])
+    with pytest.raises(ValueError):
+        MidiPiece(df=df_mod)
