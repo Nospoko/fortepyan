0182421ab6ed3d29d10492637257826e880b65f3
Samuel Janas <sneakyinsect@gmail.com>
2023-11-10 15:24:15 +0100

    add documentation, improve mkdocs

--- fortepyan/midi/structures.py
+++ fortepyan/midi/structures.py
@@ -10,6 +10,24 @@ from fortepyan.midi import tools as midi_tools
 
 @dataclass
 class MidiPiece:
+    """
+    A data class representing a piece of MIDI music, encapsulated in a Pandas DataFrame.
+
+    This class provides functionalities for managing MIDI data, including methods to manipulate and represent the MIDI piece.
+    The data is primarily stored in a Pandas DataFrame (`df`) which contains columns like 'start', 'end', 'duration',
+    'pitch', and 'velocity', essential for MIDI data representation. The class also includes source information for
+    additional context.
+
+    Attributes:
+        df (pd.DataFrame): The DataFrame containing the MIDI data.
+        source (dict, optional): Additional information about the MIDI piece's source. Defaults to None.
+
+    Examples:
+        Creating a MidiPiece instance:
+        >>> midi_piece = MidiPiece(df=my_midi_df)
+
+    """
+
     df: pd.DataFrame
     source: dict = None
 
@@ -54,24 +72,63 @@ class MidiPiece:
         return self.df.shape[0]
 
     def time_shift(self, shift_s: float):
+        """
+        Shift the start and end times of all notes in the MidiPiece by a specified amount.
+
+        This method adjusts the start and end times of every note in the MidiPiece by adding the specified shift amount
+        `shift_s`. This can be used to move the entire piece forward or backward in time.
+
+        Args:
+            shift_s (float): The amount of time (in seconds) to shift the start and end times of the notes.
+                        Positive values shift the piece forward, and negative values shift it backward.
+
+        Examples:
+            Shifting the entire piece forward by 2 seconds:
+            >>> midi_piece.time_shift(2.0)
+
+            Shifting the entire piece backward by 0.5 seconds:
+            >>> midi_piece.time_shift(-0.5)
+
+        Note:
+            - This method modifies the MidiPiece object in place and does not return a new object.
+        """
         self.df.start += shift_s
         self.df.end += shift_s
 
     def trim(self, start: float, finish: float, shift_time: bool = True, slice_type: str = "standard") -> "MidiPiece":
         """
-        Trim the MidiPiece object based on a specified slicing type.
+        Trim a segment of a MIDI piece based on specified start and finish parameters, with options for different slicing types.
+
+        This method modifies the MIDI piece by selecting a segment from it, based on the `start` and `finish` parameters.
+        The segment can be selected through different methods determined by `slice_type`. If `shift_time` is True,
+        the timing of notes in the trimmed segment will be shifted to start from zero.
 
         Args:
-        - start (float): Depending on `slice_type`, this is either the start time in seconds or the start index.
-        - finish (float): Depending on `slice_type`, this is either the end time in seconds or the end index.
-        - shift_time (bool, optional): If True, the trimmed piece's start time will be shifted to 0. Defaults to True.
-        - slice_type (str, optional): Determines the slicing method ('standard', 'by_end', 'index'). Defaults to "standard".
-            - "standard": Slices the MidiPiece to include notes that start within the [start, finish] time range.
-            - "by_end": Slices the MidiPiece to include notes where the end time is within the [start, finish] time range.
-            - "index": Slices the MidiPiece by note indices, where start and finish must be integer indices.
+            start (float | int): The starting point of the segment. It's treated as a float for 'standard' or 'by_end' slicing types,
+                                and as an integer for 'index' slicing type.
+            finish (float | int): The ending point of the segment. Similar to `start`, it's treated as a float or an integer
+                                depending on the `slice_type`.
+            shift_time (bool, optional): Whether to shift note timings in the trimmed segment to start from zero. Default is True.
+            slice_type (str, optional): The method of slicing. Can be 'standard', 'by_end', or 'index'. Default is 'standard'.
 
         Returns:
-        - MidiPiece: The trimmed MidiPiece object.
+            MidiPiece: A new `MidiPiece` object representing the trimmed segment of the original MIDI piece.
+
+        Raises:
+            ValueError: If `start` and `finish` are not integers when `slice_type` is 'index', or if `start` is larger than `finish`.
+            IndexError: If the indices are out of bounds for 'index' slicing type, or if no notes are found in the specified range for other types.
+            NotImplementedError: If the `slice_type` provided is not implemented.
+
+        Examples:
+            Trimming using standard slicing:
+            >>> midi_piece.trim(start=1.0, finish=5.0)
+
+            Trimming using index slicing:
+            >>> midi_piece.trim(start=0, finish=10, slice_type="index")
+
+            Trimming with time shift disabled:
+            >>> midi_piece.trim(start=1.0, finish=5.0, shift_time=False)
+
         """
         if slice_type == "index":
             if not isinstance(start, int) or not isinstance(finish, int):
@@ -103,6 +160,32 @@ class MidiPiece:
         return out
 
     def __sanitize_get_index(self, index: slice) -> slice:
+        """
+        Sanitize and adjust the provided slice index for the MIDI file object.
+
+        This private method ensures that the slice provided is valid for slicing a MIDI file object. It adjusts the slice
+        to handle scenarios where only one bound (start or stop) is provided. If no start is specified, it defaults to 0.
+        If no stop is specified, it defaults to the size of the MIDI file.
+
+        Parameters:
+            index (slice): The slice object to be sanitized and adjusted. It must be a slice object.
+
+        Returns:
+            slice: The sanitized and possibly adjusted slice object.
+
+        Raises:
+            TypeError: If the provided index is not a slice object.
+
+        Examples:
+            - Getting a part of the MIDI file from the beginning up to a stop point:
+                >>> midi_file.__sanitize_get_index(slice(None, 10))
+
+            - Getting a part of the MIDI file from a start point to the end:
+                >>> midi_file.__sanitize_get_index(slice(5, None))
+
+        Note:
+        - This method is intended for internal use within the class and should not be called directly from outside the class.
+        """
         if not isinstance(index, slice):
             raise TypeError("You can only get a part of MidiFile that has multiple notes: Index must be a slice")
 
@@ -117,6 +200,36 @@ class MidiPiece:
         return index
 
     def __getitem__(self, index: slice, shift_time: bool = True) -> "MidiPiece":
+        """
+        Get a slice of the MIDI piece, optionally shifting the time of notes.
+
+        This method returns a segment of the MIDI piece based on the provided index. It sanitizes the index using the
+        `__sanitize_get_index` method. If `shift_time` is True, it shifts the start and end times of the notes in the
+        segment so that the first note starts at time 0. The method also keeps track of the original piece's information
+        in the sliced piece's source data.
+
+        Args:
+            index (slice): The slicing index to select a part of the MIDI piece. It must be a slice object.
+            shift_time (bool, optional): If True, shifts the start and end times of notes so the first note starts at 0.
+                                    Default is True.
+
+        Returns:
+            MidiPiece: A new `MidiPiece` object representing the sliced segment of the original MIDI piece.
+
+        Raises:
+            TypeError: If the provided index is not a slice object (handled in `__sanitize_get_index`).
+
+        Examples:
+            Getting a slice from the MIDI file with time shift:
+                >>> midi_piece[0:10]
+
+            Getting a slice without time shift:
+                >>> midi_piece[5:15, shift_time=False]
+
+        Note:
+            The `__getitem__` method is a special method in Python used for indexing or slicing objects. In this class,
+        it is used to get a slice of a MIDI piece.
+        """
         index = self.__sanitize_get_index(index)
         part = self.df[index].reset_index(drop=True)
 
@@ -141,6 +254,31 @@ class MidiPiece:
         return out
 
     def __add__(self, other: "MidiPiece") -> "MidiPiece":
+        """
+        Combine this MidiPiece with another MidiPiece, adjusting the time stamps.
+
+        This method overloads the `+` operator for MidiPiece objects. It concatenates the DataFrame of the current object
+        with that of another MidiPiece, adjusting the start and end times of notes in the second piece so that they follow
+        sequentially after the first piece. A warning is raised to inform the user that the resulting piece may not be
+        musically valid.
+
+        Parameters:
+            other (MidiPiece): Another MidiPiece object to add to the current one.
+
+        Returns:
+            MidiPiece: A new MidiPiece object that represents the combination of the two MidiPieces.
+
+        Raises:
+            TypeError: If the object being added is not an instance of MidiPiece.
+
+        Examples:
+            Adding two MidiPiece objects:
+            >>> combined_piece = midi_piece1 + midi_piece2
+
+        Note:
+            - The method ensures that the original MidiPiece objects are not modified during the addition.
+            - A UserWarning is raised to indicate that the resulting piece might not be musically valid.
+        """
         if not isinstance(other, MidiPiece):
             raise TypeError("You can only add MidiPiece objects to other MidiPiece objects.")
 
@@ -184,9 +322,34 @@ class MidiPiece:
         df["end"] = df.start + df.duration
         return df
 
-    def to_midi(self, track_name: str = "piano"):
+    def to_midi(self, instrument_name: str = "Acoustic Grand Piano") -> pretty_midi.PrettyMIDI:
+        """
+        Converts the note data stored in this object into a MIDI track using the specified instrument.
+
+        This function creates a MIDI track with notes defined by the object's data. It uses the pretty_midi library
+        to construct the track and the notes within it. The instrument used for the MIDI track can be specified,
+        and defaults to "Acoustic Grand Piano" if not provided.
+
+        Args:
+            instrument_name (str, optional):
+                The name of the instrument to be used for the MIDI track. This should be a valid instrument name
+                that can be interpreted by the pretty_midi library. Defaults to "Acoustic Grand Piano". See the note below for more information.
+
+        Returns:
+            pretty_midi.PrettyMIDI:
+                A PrettyMIDI object representing the MIDI track created from the note data. This object can be
+                further manipulated or directly written to a MIDI file.
+
+        Examples:
+            >>> track = my_object.to_midi("Violin")
+            This would create a MIDI track using the notes in 'my_object' with a Violin instrument.
+
+        Note:
+            - See [this wikipedia article](https://en.wikipedia.org/wiki/General_MIDI#Parameter_interpretations) for instrument names
+        """
         track = pretty_midi.PrettyMIDI()
-        piano = pretty_midi.Instrument(program=0, name=track_name)
+        program = pretty_midi.instrument_name_to_program(instrument_name)
+        instrument = pretty_midi.Instrument(program=program, name=instrument_name)
 
         # Convert the DataFrame to a list of tuples to avoid pandas overhead in the loop
         note_data = self.df[["velocity", "pitch", "start", "end"]].to_records(index=False)
@@ -194,9 +357,9 @@ class MidiPiece:
         # Now we can iterate through this array which is more efficient than DataFrame iterrows
         for velocity, pitch, start, end in note_data:
             note = pretty_midi.Note(velocity=int(velocity), pitch=int(pitch), start=start, end=end)
-            piano.notes.append(note)
+            instrument.notes.append(note)
 
-        track.instruments.append(piano)
+        track.instruments.append(instrument)
 
         return track
 
