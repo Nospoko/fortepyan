63a6dfa3934218a957d31a5d979a47aca2c1dd0b
Samuel Janas <sneakyinsect@gmail.com>
2023-11-17 13:25:18 +0100

    sustain pedal documentation

--- fortepyan/midi/tools.py
+++ fortepyan/midi/tools.py
@@ -6,6 +6,36 @@ def apply_sustain(
     sustain: pd.DataFrame,
     sustain_threshold: int = 64,
 ) -> pd.DataFrame:
+    """
+    Apply sustain pedal effects to the notes in a DataFrame.
+
+    This function uses a second DataFrame containing sustain pedal events to extend
+    the duration of notes in the original DataFrame. It modifies the end times of notes
+    that are held during the time when the sustain pedal is pressed down.
+
+    Args:
+        df (pd.DataFrame):
+            The DataFrame containing musical note data. Expected to have columns
+            'start', 'end', and 'pitch', where 'start' and 'end' represent the
+            start and end times of the notes.
+        sustain (pd.DataFrame):
+            The DataFrame containing sustain pedal events. Expected to have columns
+            'time' and 'value', where 'time' is the timestamp of the pedal event and
+            'value' is the intensity of the pedal press.
+        sustain_threshold (int, optional):
+            The threshold value above which the sustain pedal is considered to be pressed
+            down. Defaults to 64.
+
+    Returns:
+        df (pd.DataFrame):
+            The modified DataFrame with updated end times for notes affected by the
+            sustain pedal.
+
+    Notes:
+        - The sustain effect is applied by extending the end time of notes to either the
+          start of the next note with the same pitch or the time when the sustain pedal is
+          released, whichever comes first.
+    """
     # Mark sustain pedal as down or up based on threshold value
     sustain["is_down"] = sustain.value >= sustain_threshold
 
@@ -42,6 +72,26 @@ def sustain_notes(
     pedal_down: float,
     pedal_up: float,
 ) -> pd.DataFrame:
+    """
+    Extend the end times of notes affected by a sustain pedal down event.
+
+    This helper function is called by `apply_sustain` to process each group of sustain
+    pedal down events. It extends the end times of notes that are playing during the
+    sustain pedal down event.
+
+    Args:
+        df (pd.DataFrame):
+            The DataFrame containing musical note data. Expected to have columns
+            'start', 'end', and 'pitch'.
+        pedal_down (float):
+            The start time of the sustain pedal down event.
+        pedal_up (float):
+            The end time of the sustain pedal down event, indicating when the pedal is released.
+
+    Returns:
+        df (pd.DataFrame):
+            The DataFrame with updated end times for the notes affected by the sustain pedal.
+    """
     end_times = []
 
     # Select notes affected by current sustain pedal down event
