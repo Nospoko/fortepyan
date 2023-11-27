13cd53c19bd7363d431b7be952fd430835e0f80c
Samuel Janas <35274379+SamuelJanas@users.noreply.github.com>
2023-11-27 13:24:05 +0100

    MIDI-129: Try to remove PrettyMIDI dependency (#5)
    * initial try
    
    * midi for testing
    
    * container classes from prettyMIDI
    
    * most of new init
    
    * migrated functions
    
    * working midi loading
    
    * remove _midi from MidiFile
    
    * change max_ticks
    
    * Further MidiFile adaptation
    
    * move containers and helpers elsewhere
    
    * segmentize init, write to midi
    
    * Dependency removed(?)

--- fortepyan/audio/render.py
+++ fortepyan/audio/render.py
@@ -1,20 +1,21 @@
 import tempfile
 
-import pretty_midi
 from pydub import AudioSegment
 from midi2audio import FluidSynth
 
 from fortepyan.audio import soundfont
+from fortepyan.midi import structures
+from fortepyan.midi import containers as midi_containers
 
 
-def midi_to_wav(midi: pretty_midi.PrettyMIDI, wavpath: str):
+def midi_to_wav(midi: structures.MidiFile, wavpath: str):
     # This will be deleted
     tmp_midi_path = tempfile.mkstemp(suffix=".mid")[1]
 
     # Add an silent event to make sure the final notes
     # have time to ring out
     end_time = midi.get_end_time() + 0.2
-    pedal_off = pretty_midi.ControlChange(64, 0, end_time)
+    pedal_off = midi_containers.ControlChange(64, 0, end_time)
     midi.instruments[0].control_changes.append(pedal_off)
 
     midi.write(tmp_midi_path)
@@ -24,7 +25,7 @@ def midi_to_wav(midi: pretty_midi.PrettyMIDI, wavpath: str):
     synth.midi_to_audio(tmp_midi_path, wavpath)
 
 
-def midi_to_mp3(midi: pretty_midi.PrettyMIDI, mp3_path: str = None):
+def midi_to_mp3(midi: structures.MidiFile, mp3_path: str = None):
     # This will be deleted
     tmp_wav_path = tempfile.mkstemp(suffix=".wav")[1]
     midi_to_wav(midi=midi, wavpath=tmp_wav_path)
