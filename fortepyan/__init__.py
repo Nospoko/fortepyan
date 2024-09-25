6419a4c95a10bd66a3e77fe046024e6484d140ec
Tomasz Roszczynialski <roszcz@gmail.com>
2024-09-25 22:20:35 +0200

    remove previous implementation

--- fortepyan/__init__.py
+++ fortepyan/__init__.py
@@ -1,9 +1,9 @@
 import pretty_midi
 
 from fortepyan import view
-from fortepyan.midi.structures import MidiFile, MidiPiece, LegacyMidiFile
+from fortepyan.midi.structures import MidiFile, MidiPiece
 
-__all__ = ["view", "MidiFile", "MidiPiece", "LegacyMidiFile"]
+__all__ = ["view", "MidiFile", "MidiPiece"]
 __version__ = "0.3.4"
 
 # Pretty MIDI will throw an unwanted error for large files with high PPQ
